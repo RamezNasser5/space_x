e467cd0bbf5a3b36101f94fc3be3e9ad6ce3e6f9
Ramez Nasser <rameznasser55@gmail.com>
2024-05-02 11:13:57 +0300

    move images to assets and add data styles to Style

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+abstract class Style {
+  static const TextStyle titelStyle = TextStyle(
+    fontSize: 26,
+    fontFamily: 'Poppins',
+  );
+
+  static const TextStyle nameStyle = TextStyle(
+    fontSize: 18,
+    fontFamily: 'Poppins',
+  );
+
+  static const TextStyle datatStyle = TextStyle(
+    fontSize: 18,
+    fontFamily: 'Poppins-Italic',
+  );
+}
